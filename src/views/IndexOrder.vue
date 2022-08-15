69ef04bef0b99c655acf4c44776363bc0ceb804c
xjw <671173782@qq.com>
2022-08-15 11:22:11 +0800

    支付宝支付功能前端页面提交

--- src/views/IndexOrder.vue
+++ src/views/IndexOrder.vue
@@ -2,42 +2,90 @@
   <div class="order_body">
     <div class="order_header">这里是头部</div>
     <div class="order_body_content">
-      <div v-for="list in orderList.list" :key="list">
-        {{ list.id }}
-      </div>
-      <!--      <vxe-grid v-bind="gridOptions"></vxe-grid>-->
+      <vxe-grid v-bind="gridOptions"></vxe-grid>
+<!--      <a-row type="flex">-->
+<!--        <a-col :flex="6" class="order_list_header">宝贝</a-col>-->
+<!--        <a-col :flex="2" class="order_list_header">单价</a-col>-->
+<!--        <a-col :flex="2" class="order_list_header">数量</a-col>-->
+<!--        <a-col :flex="2" class="order_list_header">-->
+<!--          <a-dropdown :trigger="['click']">-->
+<!--            <a class="ant-dropdown-link" style="color:#2c3e50" @click.prevent>-->
+<!--              支付方式-->
+<!--              <DownOutlined />-->
+<!--            </a>-->
+<!--            <template #overlay>-->
+<!--              <a-menu>-->
+<!--                <a-menu-item key="0">-->
+<!--                  <a href="http://www.alipay.com/">支付宝</a>-->
+<!--                </a-menu-item>-->
+<!--                <a-menu-divider />-->
+<!--                <a-menu-item key="1">-->
+<!--                  <a href="http://www.taobao.com/">微信</a>-->
+<!--                </a-menu-item>-->
+<!--              </a-menu>-->
+<!--            </template>-->
+<!--          </a-dropdown>-->
+<!--        </a-col>-->
+<!--        <a-col :flex="2" class="order_list_header">-->
+<!--          <a-dropdown :trigger="['click']">-->
+<!--            <a class="ant-dropdown-link" style="color:#2c3e50" @click.prevent>-->
+<!--              订单状态-->
+<!--              <DownOutlined />-->
+<!--            </a>-->
+<!--            <template #overlay>-->
+<!--              <a-menu>-->
+<!--                <a-menu-item key="0">-->
+<!--                  <a href="http://www.alipay.com/">未支付</a>-->
+<!--                </a-menu-item>-->
+<!--                <a-menu-divider />-->
+<!--                <a-menu-item key="1">-->
+<!--                  <a href="http://www.taobao.com/">支付成功</a>-->
+<!--                </a-menu-item>-->
+<!--                <a-menu-divider />-->
+<!--                <a-menu-item key="3">超时已关闭</a-menu-item>-->
+<!--                <a-menu-divider />-->
+<!--                <a-menu-item key="4">订单已取消</a-menu-item>-->
+<!--                <a-menu-divider />-->
+<!--                <a-menu-item key="5">已退款</a-menu-item>-->
+<!--              </a-menu>-->
+<!--            </template>-->
+<!--          </a-dropdown>-->
+
+<!--        </a-col>-->
+<!--        <a-col :flex="4" class="order_list_header">交易时间</a-col>-->
+<!--      </a-row>-->
+<!--      <div class="order_list">-->
+<!--        <div class="order_list_number">-->
+<!--          订单号: 1576129083700552179-->
+<!--        </div>-->
+
+<!--          <a-row type="flex">-->
+<!--            <a-col :flex="3">宝贝</a-col>-->
+<!--            <a-col :flex="1">￥100:00</a-col>-->
+<!--            <a-col :flex="1">1</a-col>-->
+<!--            <a-col :flex="1">支付宝</a-col>-->
+<!--            <a-col :flex="1">已付款</a-col>-->
+<!--            <a-col :flex="2">2022-08-12 16:21:57</a-col>-->
+<!--          </a-row>-->
+
+<!--      </div>-->
     </div>
   </div>
 </template>
 
 <script>
 import aliPayApi from "@/api/aliPayApi";
-import {defineComponent, reactive} from 'vue'
+import {defineComponent, reactive} from 'vue';
+
 
 export default defineComponent({
   name: "IndexOrder",
+  components: {
+
+  },
   setup() {
-    // let orderList=reactive(null)
-    // onMounted(() => {
-    //   aliPayApi.list(2).then((response)=>{
-    //      orderList=response.data;
-    //   })
-    // })
-
-    let orderList = reactive({
-      list:[],
-    });
-    const list = async () => {
-      aliPayApi.list(2).then((response) => {
-        orderList.list=response.data.list;
-      })
-    }
-
-
     const gridOptions = reactive({
       border: true,
-      showHeaderOverflow: true,
-      showOverflow: true,
       height: 600,
       rowConfig: {
         keyField: 'id'
@@ -52,182 +100,33 @@ export default defineComponent({
         pageSize: 10
       },
       columns: [
-        {type: 'seq', width: 60},
-        {field: 'name', title: '商品', sortable: true},
-        {field: 'unit_price', title: '单价'},
-        {field: 'order_amount', title: '数量'},
-        {
-          field: 'payment_type',
-          title: '支付方式',
-          sortable: true,
-          filters: [
-            {label: '支付宝', value: '支付宝'},
-            {label: '微信', value: '微信'}
-          ],
-        },
-        {
-          field: 'order_status',
-          title: '订单状态',
-          filters: [
-            {label: '未支付', value: '未支付'},
-            {label: '已退款', value: '已退款'},
-            {label: '退款异常', value: '退款异常'},
-            {label: '支付成功', value: '支付成功'},
-            {label: '用户已取消', value: '用户已取消'},
-            {label: '超时已关闭', value: '用户已取消'},
-          ],
-          sortable: true,
-        },
-        {
-          field: 'create_time',
-          title: '交易时间',
-          sortable: true,
-        },
-        // { field: 'address', title: 'Address', showOverflow: true }
+        { field: 'order_no', title: '订单号' },
+        { field: 'title', title: '订单' },
+        { field: 'price', title: '单价' },
+        { field: 'total', title: '数量' },
+        { field: 'total_fee', title: '实付款' },
+        { field: 'payment_type', title: '支付方式' },
+        { field: 'order_status', title: '订单状态' },
+        { field: 'update_time', title: '交易时间'}
       ],
       proxyConfig: {
-
         seq: true, // 启用动态序号代理（分页之后索引自动计算为当前页的起始序号）
-        sort: true, // 启用排序代理，当点击排序时会自动触发 query 行为
-        filter: true,
         props: {
           result: 'result',
           total: 'page.total'
         },
         ajax: {
           // 接收 Promise
-          query: ({page}) => {
+          query: ({ page }) => {
             return new Promise(resolve => {
+              gridOptions.loading = true
               setTimeout(() => {
+               gridOptions.loading = false
                 const list = [
-                  {
-                    name: 'Test1',
-                    unit_price: 'T1',
-                    order_amount: 'Develop',
-                    payment_type: 'Man',
-                    order_status: 28,
-                    create_time: 'Shenzhen'
-                  },
-                  {
-                    name: 'Test1',
-                    unit_price: 'T1',
-                    order_amount: 'Develop',
-                    payment_type: 'Man',
-                    order_status: 28,
-                    create_time: 'Shenzhen'
-                  },
-                  {
-                    name: 'Test1',
-                    unit_price: 'T1',
-                    order_amount: 'Develop',
-                    payment_type: 'Man',
-                    order_status: 28,
-                    create_time: 'Shenzhen'
-                  },
-                  {
-                    name: 'Test1',
-                    unit_price: 'T1',
-                    order_amount: 'Develop',
-                    payment_type: 'Man',
-                    order_status: 28,
-                    create_time: 'Shenzhen'
-                  },
-                  {
-                    name: 'Test1',
-                    unit_price: 'T1',
-                    order_amount: 'Develop',
-                    payment_type: 'Man',
-                    order_status: 28,
-                    create_time: 'Shenzhen'
-                  },
-                  {
-                    name: 'Test1',
-                    unit_price: 'T1',
-                    order_amount: 'Develop',
-                    payment_type: 'Man',
-                    order_status: 28,
-                    create_time: 'Shenzhen'
-                  },
-                  {
-                    name: 'Test1',
-                    unit_price: 'T1',
-                    order_amount: 'Develop',
-                    payment_type: 'Man',
-                    order_status: 28,
-                    create_time: 'Shenzhen'
-                  },
-                  {
-                    name: 'Test1',
-                    unit_price: 'T1',
-                    order_amount: 'Develop',
-                    payment_type: 'Man',
-                    order_status: 28,
-                    create_time: 'Shenzhen'
-                  },
-                  {
-                    name: 'Test1',
-                    unit_price: 'T1',
-                    order_amount: 'Develop',
-                    payment_type: 'Man',
-                    order_status: 28,
-                    create_time: 'Shenzhen'
-                  },
-                  {
-                    name: 'Test1',
-                    unit_price: 'T1',
-                    order_amount: 'Develop',
-                    payment_type: 'Man',
-                    order_status: 28,
-                    create_time: 'Shenzhen'
-                  },
-                  {
-                    name: 'Test1',
-                    unit_price: 'T1',
-                    order_amount: 'Develop',
-                    payment_type: 'Man',
-                    order_status: 28,
-                    create_time: 'Shenzhen'
-                  },
-                  {
-                    name: 'Test1',
-                    unit_price: 'T1',
-                    order_amount: 'Develop',
-                    payment_type: 'Man',
-                    order_status: 28,
-                    create_time: 'Shenzhen'
-                  },
-                  {
-                    name: 'Test1',
-                    unit_price: 'T1',
-                    order_amount: 'Develop',
-                    payment_type: 'Man',
-                    order_status: 28,
-                    create_time: 'Shenzhen'
-                  },
-                  {
-                    name: 'Test1',
-                    unit_price: 'T1',
-                    order_amount: 'Develop',
-                    payment_type: 'Man',
-                    order_status: 28,
-                    create_time: 'Shenzhen'
-                  },
-                  {
-                    name: 'Test1',
-                    unit_price: 'T1',
-                    order_amount: 'Develop',
-                    payment_type: 'Man',
-                    order_status: 28,
-                    create_time: 'Shenzhen'
-                  },
-                  {
-                    name: 'Test1',
-                    unit_price: 'T1',
-                    order_amount: 'Develop',
-                    payment_type: 'Man',
-                    order_status: 28,
-                    create_time: 'Shenzhen'
-                  }
+                  { order_no: 'ORDER_20220812162010629', title: '商品1', price: '￥10.00', total: '3', total_fee: '￥30.00', payment_type: '支付宝', order_status: '已付款',update_time:"2022-08-12 16:53:03" },
+                  { order_no: 'ORDER_20220812162010629', title: '商品1', price: '￥10.00', total: '3', total_fee: '￥30.00', payment_type: '支付宝', order_status: '已付款',update_time:"2022-08-12 16:53:03" },
+                  { order_no: 'ORDER_20220812162010629', title: '商品1', price: '￥10.00', total: '3', total_fee: '￥30.00', payment_type: '支付宝', order_status: '已付款',update_time:"2022-08-12 16:53:03" },
+                  { order_no: 'ORDER_20220812162010629', title: '商品1', price: '￥10.00', total: '3', total_fee: '￥30.00', payment_type: '支付宝', order_status: '已付款',update_time:"2022-08-12 16:53:03" },
                 ]
                 resolve({
                   page: {
@@ -242,7 +141,17 @@ export default defineComponent({
       }
     })
 
-    list()
+    let orderList = reactive({
+      list:[],
+    });
+    const list = async () => {
+      aliPayApi.list(2).then((response) => {
+        orderList.list=response.data.list;
+      })
+    }
+
+
+  //  list()
     return {
       gridOptions,
       orderList,
@@ -270,6 +179,7 @@ export default defineComponent({
     position: absolute;
     left: 20vw;
     top: 100px;
+    padding: 5px 10px 5px 10px;
     @include diy_size_minHeight(60vw, 800px);
     background: #ffffff;
     border: {
